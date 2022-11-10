521c74daa1db5809e4455f5b5c12dd808d17fdf1
gerryron <gerryron02@gmail.com>
2022-11-10 13:51:26 +0700

    Setup Eslint, Prettier, Husky

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express, { Application, Request, Response, NextFunction } from 'express';
 const app: Application = express();
 const port: Number = 4000;
 
-app.use('/', (req: Request, res: Response, next: NextFunction) => {
+app.use('/health', (req: Request, res: Response, next: NextFunction) => {
   res.status(200).send({ data: 'Hello World' });
 });
 
